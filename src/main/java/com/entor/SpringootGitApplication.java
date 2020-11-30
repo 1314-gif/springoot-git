1aa59b32ea161fd520d4c884ba6d9ed0414b7b1e
Administrator <2643670904@qq.com>
2020-11-30 15:34:37 +0800

    test

---
+++ src/main/java/com/entor/SpringootGitApplication.java
@@ -0,0 +1,13 @@
+package com.entor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringootGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringootGitApplication.class, args);
+    }
+
+}
